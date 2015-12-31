78d2466eece01b406643e5a2619ae995adb94191
Keith Holloway <keith@eastfive.com>
2015-12-31 14:32:03 -0600

    Genesis

---
+++ BlackBarLabs.Search.Azure.Tests/AzureSearchEngineTests.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BlackBarLabs.Search.Azure.Tests
+{
+    [TestClass]
+    public class AzureSearchEngineTests
+    { 
+        private AzureSearchEngine azureSearchEngine;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            var engines = new SearchEngines("SearchServiceName", "SearchServiceApiKey");
+            azureSearchEngine = engines.AzureSearchEngine;
+        }
+
+        [TestMethod]
+        public async Task CreateIndex()
+        {
+            var distributorId = Guid.NewGuid().ToString();
+            await CreateIndexInternalAsync(distributorId);
+            await DeleteIndexInternalAsync(distributorId);
+        }
+
+        [TestMethod]
+        public async Task DeleteIndex()
+        {
+            var exception = default(Exception);
+            var distributorId = Guid.NewGuid().ToString();
+            try
+            {
+                await CreateIndexInternalAsync(distributorId);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            finally
+            {
+                await DeleteIndexInternalAsync(distributorId);
+                if (default(Exception) != exception)
+                    throw exception;
+            }
+
+        }
+
+        [TestMethod]
+        public async Task AddDataToIndex()
+        {
+            var exception = default(Exception);
+            var distributorId = Guid.NewGuid().ToString("N");
+            try
+            {
+                await CreateIndexInternalAsync(distributorId);
+                var products = CreateProductList();
+                await azureSearchEngine.IndexItemsAsync<Product>(distributorId, products, async indexName =>
+                {
+                    await CreateIndexInternalAsync(indexName);
+                });
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            finally
+            {
+                await DeleteIndexInternalAsync(distributorId);
+                if (default(Exception) != exception)
+                    throw exception;
+            }
+        }
+
+        [TestMethod]
+        public async Task Search()
+        {
+            var exception = default(Exception);
+            var distributorId = Guid.NewGuid().ToString();
+            try
+            {
+                await CreateIndexInternalAsync(distributorId);
+                var products = CreateProductList();
+                await azureSearchEngine.IndexItemsAsync<Product>(distributorId, products, async indexName =>
+                {
+                    await CreateIndexInternalAsync(indexName);
+                });
+                await Task.Delay(5000);  // Azure Search says the indexing on their side could take some time.  Particularly on a shared search instance.
+                var foundDocs = await azureSearchEngine.SearchDocumentsAsync<Product>(distributorId, "Yellow",
+                    product => product);
+                var found = false;
+                foreach (var doc in foundDocs)
+                {
+                    if (doc.ProductName.Contains("Yellow"))
+                        found = true;
+                }
+                Assert.IsTrue(found);
+            }
+            catch (Exception ex)
+            {
+                exception = ex;
+            }
+            finally
+            {
+                await DeleteIndexInternalAsync(distributorId);
+                if (default(Exception) != exception)
+                    throw exception;
+            }
+        }
+
+        private async Task CreateIndexInternalAsync(string distributorId)
+        {
+            var result = await azureSearchEngine.CreateIndexAsync(distributorId, field =>
+            {
+                foreach (var fieldInfo in ProductFieldInfo.SearchFields)
+                {
+                    field.Invoke(fieldInfo.Name, fieldInfo.Type, fieldInfo.IsKey, fieldInfo.IsSearchable, fieldInfo.IsFilterable, fieldInfo.IsSortable, fieldInfo.IsFacetable, fieldInfo.IsRetrievable);
+                }
+            }, 5000);
+            Assert.IsTrue(result);
+        }
+
+        private async Task DeleteIndexInternalAsync(string distributorId)
+        {
+            var result = await azureSearchEngine.DeleteIndexAsync(distributorId);
+            Assert.IsTrue(result);
+        }
+
+        private static List<Product> CreateProductList()
+        {
+            var products = new List<Product>
+            {
+                new Product() {RowKey = "1", Brand = "Coke", ProductName = "Coke Classic", Sku = "123456", Cost = "100"},
+                new Product() {RowKey = "2", Brand = "Coke", ProductName = "Sprite", Sku = "123457", Cost = "100"},
+                new Product() {RowKey = "3", Brand = "Coke", ProductName = "Diet Coke", Sku = "123458", Cost = "100"},
+                new Product() {RowKey = "4", Brand = "Coke", ProductName = "Mello Yellow", Sku = "123459", Cost = "100"}
+            };
+            return products;
+        } 
+    }
+}
---
+++ BlackBarLabs.Search.Azure/AzureSearchEngine.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Hyak.Common;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+
+namespace BlackBarLabs.Search.Azure
+{
+    public class AzureSearchEngine
+    {
+        private SearchServiceClient searchClient;
+
+        public AzureSearchEngine(SearchServiceClient searchClient)
+        {
+            this.searchClient = searchClient;
+        }
+
+        public delegate void CreateIndexFieldsCallback(CreateFieldCallback createField);
+        public delegate void CreateFieldCallback(string fieldName, string fieldType, bool isKey, bool isSearchable, bool isFilterable, bool isSortable, bool isFacetable, bool isRetrievable);
+        public async Task<bool> CreateIndexAsync(string indexName, CreateIndexFieldsCallback createIndexFieldsCallback, int creationDelay = 0)
+        {
+            try
+            {
+                var fields = new List<Field>();
+                createIndexFieldsCallback.Invoke(
+                    (name, type, isKey, isSearchable, isFilterable, isSortable, isFacetable, isRetrievable) =>
+                    {
+                        fields.Add(new Field()
+                        {
+                            Name = name,
+                            Type = GetEdmType(type),
+                            IsKey = isKey,
+                            IsSearchable = isSearchable,
+                            IsFilterable = isFilterable,
+                            IsSortable = isSortable,
+                            IsFacetable = isFacetable,
+                            IsRetrievable = isRetrievable
+                        });
+                    });
+
+                var definition = new Index()
+                {
+                    Name = indexName,
+                    Fields = fields
+                };
+                var response = await searchClient.Indexes.CreateAsync(definition);
+                await Task.Delay(creationDelay);
+                return (response.StatusCode == HttpStatusCode.Created || response.StatusCode == HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Error creating index: {ex.Message}\r\n");
+            }
+        }
+
+        public bool CreateIndex(string indexName, CreateIndexFieldsCallback createIndexFieldsCallback)
+        {
+            try
+            {
+                var fields = new List<Field>();
+                createIndexFieldsCallback.Invoke(
+                    (name, type, isKey, isSearchable, isFilterable, isSortable, isFacetable, isRetrievable) =>
+                    {
+                        fields.Add(new Field()
+                        {
+                            Name = name,
+                            Type = GetEdmType(type),
+                            IsKey = isKey,
+                            IsSearchable = isSearchable,
+                            IsFilterable = isFilterable,
+                            IsSortable = isSortable,
+                            IsFacetable = isFacetable,
+                            IsRetrievable = isRetrievable
+                        });
+                    });
+
+                var definition = new Index()
+                {
+                    Name = indexName,
+                    Fields = fields
+                };
+                var response = searchClient.Indexes.Create(definition);
+                return (response.StatusCode == HttpStatusCode.Created || response.StatusCode == HttpStatusCode.OK);
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Error creating index: {ex.Message}\r\n");
+            }
+        }
+
+        private static string GetEdmType(string type)
+        {
+            // Types of search fields must be in Entity Data Format.  https://msdn.microsoft.com/en-us/library/azure/dn946880.aspx
+            switch (type)
+            {
+                case "System.String":
+                    return "Edm.String";
+                case "System.Decimal":
+                    return "Edm.String";
+            }
+            return "";
+        }
+
+        public async Task<bool> DeleteIndexAsync(string indexName)
+        {
+            try
+            {
+                await searchClient.Indexes.DeleteAsync(indexName);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool DeleteIndex(string indexName)
+        {
+            try
+            {
+                searchClient.Indexes.Delete(indexName);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public async Task<bool> IndexItemsAsync<T>(string indexName, List<T> itemList, Action<string> createIndex, int numberOfTimesToRetry = 200)
+            where T : class
+        {
+
+            if (! searchClient.Indexes.Exists(indexName))
+            {
+                createIndex.Invoke(indexName);
+            }
+
+            //TODO - Use async methods on search api   http://alexmang.com/2015/03/azure-search-is-now-generally-available/
+
+            var indexClient = searchClient.Indexes.GetClient(indexName);
+            if (default(SearchIndexClient) == indexClient)
+                throw new InvalidOperationException("Index does not exist: " + indexName);
+
+            while (true)
+            {
+                try
+                {
+                    var response =
+                        await indexClient.Documents.IndexAsync(IndexBatch.Create(itemList.Select(IndexAction.Create)));
+                    return (response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Created);
+                }
+                catch (Exception ex)
+                {
+                    if((typeof(IndexBatchException) != ex.GetType()) && (typeof(CloudException) != ex.GetType()))
+                        throw;
+                }
+                numberOfTimesToRetry--;
+
+                if (numberOfTimesToRetry <= 0)
+                    throw new Exception("Indexing of items has exceeded maximum allowable attempts");
+            }
+        }
+        
+        public async Task<IEnumerable<TResult>> SearchDocumentsAsync<TResult>(string indexName, string searchText, Func<TResult, TResult> convertFunc, string filter = null)
+            where TResult : class, new()
+        {
+            var indexClient = searchClient.Indexes.GetClient(indexName);
+
+            // Execute search based on search text and optional filter 
+            var sp = new SearchParameters();
+            if (!string.IsNullOrEmpty(filter))
+            {
+                sp.Filter = filter;
+            }
+            var response = await indexClient.Documents.SearchAsync<TResult>(searchText, sp);
+            //var products = response.Select((result => result.Document));
+            var products = response.Select(item => convertFunc(item.Document));
+            return products;
+        }
+    }
+}
